6fee188502744b771f918f56a18d2642f26056d9
Freddie Nelson <freddie0208@hotmail.com>
2020-11-17 19:52:31 +0000

    init

---
+++ client/js/main.js
@@ -0,0 +1,75 @@
+const images = document.getElementById("images")
+const corsLink = "http://127.0.0.1:3000/";
+const count = 30
+
+function generateAll() {
+  for (let i = 0; i < count; i++) {
+    generate();
+  }
+}
+
+async function generate() {
+  const code = getCode();
+  const a = document.createElement("a");
+  a.classList.add("image", "placeholder");
+  images.appendChild(a);
+
+
+  try {
+    const [ image, link ] = await getImage(code);
+
+    a.appendChild(image);
+    a.setAttribute("href", link);
+    a.setAttribute("target", "_blank")
+    a.classList.remove("placeholder");
+  } catch {
+    images.removeChild(a);
+    console.log("failed to get image")
+  }
+}
+
+async function getImage(code) {
+  const res = await fetch(`${corsLink}https://prnt.sc/${code}`);
+  const html = await res.text();
+
+  const regex = /<meta property="og:image" content="(.*)\.png"/
+  const url = html.match(regex)[0].split("content=\"")[1].split("\"")[0];
+
+  if (url.startsWith("//st")) return;
+
+  const image = new Image(400, 400);
+  const blob = await (await fetch(corsLink + url)).blob();
+
+  const fr = new FileReader();
+
+  return new Promise((resolve) => {
+    fr.onloadend = f => {
+      image.src = fr.result;
+      resolve([ image, url ]);
+    };
+
+    fr.readAsDataURL(blob);
+  })
+}
+
+function getCode() {
+  const chars = randomChars();
+  let nums = '';
+
+  for (let i = 0; i < 4; i++) {
+      nums += `${Math.floor(Math.random() * 10)}`;
+  }
+
+  return chars + nums;
+}
+
+function randomChars() {
+    const chars = "abcdefghijklmnopqrstuvwxyz";
+    let result = '';
+
+    for (let i = 0; i < 2; i++) {
+        result += chars[Math.floor(Math.random() * 26)]
+    }
+
+    return result
+}
---
+++ server/main.js
@@ -0,0 +1,33 @@
+const http = require("http");
+const fetch = require("node-fetch");
+const datauri = require("datauri");
+
+const hostname = "127.0.0.1";
+const port = 3000;
+
+const server = http.createServer(async (req, res) => {
+  // Set CORS headers
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Request-Method', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+	res.setHeader('Access-Control-Allow-Headers', req.headers.origin);
+
+  res.statusCode = 200;
+
+  const imageURL = req.url.substring(1, req.url.length);
+  let response;
+
+  if (imageURL.includes(".png")) {
+    res.setHeader("Content-Type", "image/png");
+    response = await (await fetch(imageURL)).buffer()
+  } else {
+    res.setHeader("Content-Type", "text/html");
+    response = await (await fetch(imageURL)).text();
+  }
+
+  res.end(response);
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
