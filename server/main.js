52f7516167d4906e01b1ce5c3b12d5f18731b5bb
Freddie Nelson <freddie0208@hotmail.com>
2020-11-17 21:26:53 +0000

    change cors

--- server/main.js
+++ server/main.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 
 const server = https.createServer(async (req, res) => {
   // Set CORS headers
-	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Origin', 'https://prntscgenerator.ml');
 	res.setHeader('Access-Control-Request-Method', '*');
 	res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
 	res.setHeader('Access-Control-Allow-Headers', req.headers.origin);
